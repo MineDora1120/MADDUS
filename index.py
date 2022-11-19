a5e8b29be48e99bc0c1eec1ed941da86505f1264
MADO <mydodo1120@naver.com>
2022-11-19 22:04:42 +0900

    Copy,Paste

--- index.py
+++ index.py
@@ -4,9 +4,9 @@ import win32api
 import pyautogui
 import time
 
-print('MADDUS 1.0.12-E8 실행중, Made By Mado!')
+print('MADDUS 1.0.12-CV 실행중, Made By Mado!')
 print('\n\n프로그램이 시작되었어요!')
-print('마우스 하를 누르면 F8이, 마우스 상을 누르면 ESC가 실행되요.')
+print('마우스 하를 누르면 붙혀넣기가, 마우스 상을 누르면 복사가 되요.')
 while True:
     time.sleep(0.1)
     DC = win32api.GetAsyncKeyState(0x05)
@@ -14,10 +14,10 @@ while True:
     
     #print(click)
     if DC < 0:
-        print('F8이 실행되었어요.')
-        pyautogui.press('F8')
+        print('붙혀넣어졌어요,')
+        pyautogui.hotkey('ctrl', 'v')
 
     if UC < 0:
-        print('ESC가 실행되었어요.')
-        pyautogui.press('ESC')
+        print('복사 되었어요!')
+        pyautogui.hotkey('ctrl', 'c')
     
